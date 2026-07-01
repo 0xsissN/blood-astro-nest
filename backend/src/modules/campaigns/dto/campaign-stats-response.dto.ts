d97d1d78b5678e3f74abfe334354e400c3fefeb8
SSS-C-C <sarah.chavez@ucb.edu.bo>
2026-06-30 21:10:00 -0400

    Identificar campañas más exitosas

---
+++ backend/src/modules/campaigns/dto/campaign-stats-response.dto.ts
@@ -0,0 +1,10 @@
+export class CampaignStatsResponseDto {
+  id!: number;
+  nombre!: string;
+  lugar!: string;
+  fechaInicio!: string;
+  fechaFin!: string;
+  activa!: boolean;
+  totalDonaciones!: number;
+  totalMl!: number;
+}
